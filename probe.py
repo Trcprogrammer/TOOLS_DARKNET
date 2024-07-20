a667488abb4a023806915bbceb1f16c219fa96b3
tracy <tiritord@gmail.com>
2024-07-20 05:53:07 +0200

    2

--- probe.py
+++
@@ -1,6 +0,0 @@
-import subprocess as sb
-
-options = input("Enter the options you want to use for the program:\n")
-
-if options == '1':
-    sb.call(["python3", "darkdump.py", "-q", "hack"], cwd="get_dark/darkdump-main")
